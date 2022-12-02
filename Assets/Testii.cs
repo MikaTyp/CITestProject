beac5155a9f60393f56b9f4fc1f0e126a146b1bf
MikaTyp <95697961+MikaTyp@users.noreply.github.com>
2022-12-02 10:21:52 +0200

    test push

---
+++ Assets/Testii.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CloudKayakLabs.TextColorManager;
+
+public class Testii : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
